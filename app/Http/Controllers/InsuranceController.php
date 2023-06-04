355265fa390c13003c496ab9fad20d3cf68da540
Jetro Macalipay <jetromacalipay@gmail.com>
2023-06-05 04:19:32 +0800

    te

--- app/Http/Controllers/InsuranceController.php
+++ app/Http/Controllers/InsuranceController.php
@@ -7,79 +7,56 @@ use Illuminate\Http\Request;
 
 class InsuranceController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        return view('backend.pages.setup.payroll_setup.work_assignments');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    public function get() {
+        if(request()->ajax()) {
+            return datatables()->of(Insurance::get())
+            ->addIndexColumn()
+            ->make(true);
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $validate = $request->validate([
+            'title' => 'required',
+            'status' => 'required',
+        ]);
+
+        $request['workstation_id'] = Auth::user()->workstation_id;
+        $request['created_by'] = Auth::user()->id;
+        $request['updated_by'] = Auth::user()->id;
+
+        Insurance::create($request->all());
+
+        return response()->json(compact('validate'));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Insurance  $insurance
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Insurance $insurance)
+    public function edit($id)
     {
-        //
+        $work_assignment = Insurance::where('id', $id)->orderBy('id')->firstOrFail();
+        return response()->json(compact('work_assignment'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Insurance  $insurance
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Insurance $insurance)
+    public function update(Request $request, $id)
     {
-        //
+        $request['updated_by'] = Auth::user()->id;
+        Insurance::find($id)->update($request->all());
+        return "Record Saved";
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Insurance  $insurance
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Insurance $insurance)
+    public function destroy(Request $request)
     {
-        //
-    }
+        $record = $request->data;
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Insurance  $insurance
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Insurance $insurance)
-    {
-        //
+        foreach($record as $item) {
+            Insurance::find($item)->delete();
+        }
+
+        return 'Record Deleted';
     }
 }
